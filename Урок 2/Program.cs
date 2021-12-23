eb261c9db122f45274afdc66b23fcae2c0332d3d
Netalichka <netalichka2@gmail.com>
2021-12-23 14:15:51 +0300

    Update Program.cs
    step 1

--- Урок 2/Program.cs
+++ Урок 2/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int min;
+if (a < b & a < c)
+{
+    Console.WriteLine("min= ", a);
+}
+    if (b<c)
+{
+    Console.WriteLine("min= ", b);
+  
+}
+    else
+{
+    Console.WriteLine("min= ", c);
+}
