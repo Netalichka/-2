4f45bdab27f9926752a327040d890ecaa267077d
Netalichka <netalichka2@gmail.com>
2022-02-17 14:41:20 +0300

    addcomment

--- Урок 2/Program.cs
+++ Урок 2/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+//Наталья Ракитина. Урок 2
+// Написать метод, возвращающий минимальное из трёх чисел.
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 int c = Convert.ToInt32(Console.ReadLine());
