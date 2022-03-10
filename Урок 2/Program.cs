0d84c334db828e9feeb72331a9d7dc07bc3eb132
Netalichka <netalichka2@gmail.com>
2022-03-10 19:33:56 +0300

    change1003

--- Урок 2/Program.cs
+++ Урок 2/Program.cs
@@ -1,39 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-//Наталья Ракитина. Урок 4. Массивы
+Console.WriteLine("Наталья Ракитина. Урок 2");
+Console.WriteLine("Написать метод, возвращающий минимальное из трёх чисел.");
+Console.WriteLine("=========================================================");
+int a = int.Parse(Console.readline());
+int b = int.Parse(Console.readline());
+int c = int.Parse(Console.readline());
+int min = 0;
+if (a < b & a < c)
+{
+    min = a;
+
+}
+if (b < c)
+{
+    min = b;
 
 
+}
+else
+{
+    min = c;
 
-
-
-
-
-
-
-
-
-
-//Наталья Ракитина. Урок 2
-// Написать метод, возвращающий минимальное из трёх чисел.
-//int a = Convert.ToInt32(Console.ReadLine());
-//int b = Convert.ToInt32(Console.ReadLine());
-//int c = Convert.ToInt32(Console.ReadLine());
-//int min = 0;
-//if (a < b & a < c)
-//{
-//    min = a;
-    
-//}
-//    if (b<c)
-//{
-//    min = b;
-  
-  
-//}
-//    else
-//{
-//    min = c;
-    
-//}
-//Console.WriteLine($"min={min} ");
+}
+console.writeline($"min={min} ");
